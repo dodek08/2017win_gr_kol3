99183c18f2d2170c5c52452c61331221a8293d1e
Dominik Kasperski <kasperski.dominik@gmail.com>
2017-12-06 16:58:19 +0100

    task finished

--- kol1_test.py
+++ kol1_test.py
@@ -7,20 +7,67 @@
 ######################################
 
 import unittest
-from kol1 import Client
+import kol1
 
-class MyTest(unittest.TestCase):
+class ClientTest(unittest.TestCase):
 
 
 	def setUp(self):
-		self.name = "Adam"
-		self.surname = "Abacki"
-		self.cash = 0
-		self.id_number = 0
+		self.client = kol1.Client("Adam","Abacki",1000,21)
 
-	def	test_init(self):
-		self.assertEqual(self.test_instance.name, self.list_data)
-		self.assertEqual(self.test_instance.surname, self.surname)
-		self.assertEqual(self.test_instance.cash, self.cash)
-		self.assertEqual(self.test_instance.id_number, self.id_number)
-		
+	def test_init(self):
+		self.assertEquals(self.client.name,'Adam')
+		self.assertEquals(self.client.surname,'Abacki')
+		self.assertEquals(self.client.cash, 1000)
+		self.assertEquals(self.client.id_number, 21)
+
+class  BankTest(unittest.TestCase):
+
+
+	def setUp(self):
+		self.bank = kol1.Bank('PKO')
+
+	def test_init(self):
+		self.assertEquals(self.bank.name,'PKO')
+		self.assertEquals(self.bank.clients,[])
+
+	def test_new_client(self):
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.assertEquals(self.bank.clients[0].name, 'Barbara')
+		self.assertEquals(self.bank.clients[0].surname, 'Babacka')
+		self.assertEquals(self.bank.clients[0].cash, 1000)
+		self.assertEquals(self.bank.clients[0].id_number, 1)
+
+	def test_client_list(self):
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.assertEquals(self.bank.client_list(),None)
+
+	def test_cash_input(self):
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.bank.cash_input(0,100)
+		self.assertEquals(self.bank.clients[0].cash, 1100)
+
+	def test_cash_withdrawal(self):
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.bank.cash_withdrawal(0, 100)
+		self.assertEquals(self.bank.clients[0].cash, 900)
+
+	def test_money_transfer(self):
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.bank.new_client('Celina','Cabacka', 2000)
+		self.bank.money_transfer(0,1,100)
+		self.assertEquals(self.bank.clients[0].cash, 1100)
+		self.assertEquals(self.bank.clients[1].cash, 1900)
+
+class  TransferTest(unittest.TestCase):
+
+	def setUp(self):
+		self.transfer = kol1.Transfer()
+		self.bank = kol1.Bank('PKO')
+		self.bank.new_client('Barbara','Babacka', 1000)
+		self.bank.new_client('Celina','Cabacka', 2000)
+
+	def test_money_transfer(self):
+		self.transfer.money_transfer('PKO', 1, 'PKO', 2, 100)
+		self.assertEquals(self.bank.clients[0].cash, 900)
+		self.assertEquals(self.bank.clients[1].cash, 2100)
